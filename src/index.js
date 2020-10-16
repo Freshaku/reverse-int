018b89f8c700b73d767d555b26ae7174eb65d6b0
Freshaku <52348428+Freshaku@users.noreply.github.com>
2020-10-16 22:41:59 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let reverseNum = String( Math.abs(n)).split("").reverse().join("");
+    if(reverseNum[0] == 0){
+        reverseNum = reverseNum.substr(1, reverseNum.length);
+    } else{
+        return reverseNum;
+    }
+    return Number(reverseNum);
 }
